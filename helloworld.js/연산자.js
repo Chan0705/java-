a7f915da9a15a998630e06e103c95928f77b2d9f
Chan0705 <vicg0923@gmail.com>
2025-06-10 10:51:13 +0900

    java

---
+++ helloworld.js/연산자.js
@@ -0,0 +1,20 @@
+const a = 1;
+const b = '1';
+const c = 2;
+const d = 1;
+
+//비교 연산자
+//console.log('a == b', a == b); // true
+//console.log('a === b', a == b); // false
+//console.log('a < c', a < c); // true
+//console.log('a > c', a < c); // false
+//console.log('a <= d', a <= d); // true
+//console.log('a >= d', a >= d); // false
+
+
+//증감 연산자
+let e = 1;
+e++;
+console.log('e++', e++);
+e--;
+console.log('e--', e);
