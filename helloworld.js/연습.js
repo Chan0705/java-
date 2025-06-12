09d393bae71ed17dc6ae61993d4a3fe3139e762e
Chan0705 <vicg0923@gmail.com>
2025-06-13 00:42:29 +0900

    js 연습습

--- helloworld.js/연습.js
+++ helloworld.js/연습.js
@@ -1,15 +1,15 @@
-const array = [1, 2, 3, 4, 5, 6];
+// const array = [1, 2, 3, 4, 5, 6];
 
-array.push(7)
+// array.push(7)
 
-array.pop()
-array.unshift(7)
-array.splice(0, 0, 8)
+// array.pop()
+// array.unshift(7)
+// array.splice(0, 0, 8)
 
 
-const array2 = array
+// const array2 = array
 
-console.log(array2)
+// console.log(array2)
 
 
 
@@ -21,3 +21,20 @@ console.log(array2)
 
 // const anyway = array
 // console.log(anyway);
+
+
+
+// const score = [];
+
+// for (let i = '0'; i < 101; i++) {
+//     score.push(i);
+// }
+
+// function isPass(score) {
+//     // (let j = 0; j < score.length; i++);
+//     if (score >= 60) {
+//         return console.log(score >= 60, '합격');
+//     } else {
+//         return console.log(score < 60, '불합격');
+//     }
+// }
