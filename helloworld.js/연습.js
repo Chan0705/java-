f2846e432636b36d6ae25828d734329ebdaacfbd
Chan0705 <vicg0923@gmail.com>
2025-06-13 11:30:58 +0900

    jss

--- helloworld.js/연습.js
+++ helloworld.js/연습.js
@@ -38,3 +38,40 @@
 //         return console.log(score < 60, '불합격');
 //     }
 // }
+
+
+// const testscore = [71, 41, 23, 39, 87, 99]
+// testscore.forEach(function (score, i) { // function(배열요소, 배열의 인덱스)
+//     if (score >= 60) {
+//         console.log(i + 1 + "번은", "합격");
+//     }
+//     else {
+//         console.log(i + 1 + "번은", "불합격");
+//     }
+// })
+
+// let numbers = [12, 7, 22, 35, 40, 55, 68, 91, 100];
+
+// numbers.forEach(function (num, i) {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// });
+
+// let words = ["javascript", "html", "css", "react", "node"];
+
+// words.forEach(function (word, i) {
+//     let upperword = word.toUpperCase()
+//     console.log(upperword);
+// });
+
+
+const score = [51, 25, 65, 80, 97, 77]
+
+score.forEach(function (num, i) {
+    const average = score.reduce((acc, cur) => {
+        return acc + cur;
+    })
+})
+
+console.log(average);
